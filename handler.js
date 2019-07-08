f3499354cb67abac63b92ab04feac5998fc3577f
priyaparshu <priyaparshu@gmail.com>
2019-07-08 16:25:07 -0500

    Integrate s3 with sns

---
+++ handler.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const SNS = new AWS.SNS();
+
+
+module.exports.hello = (event, context, callback) => {
+  var bucket = event.Records[0].s3.bucket.name;
+  var fileName = event.Records[0].s3.object.key;
+
+  console.log(bucket)
+  console.log(fileName)
+
+  const params = {
+    Message: `A new file: ${fileName}has been uploaded to the bucket: ${bucket} `,
+    TopicArn: "arn:aws:sns:us-east-1:389480544174:s3uploadTopic"
+  }
+  //console.log("params", params)
+  SNS.publish(params, function (err, data) {
+    if (err) {
+      console.error(err.stack);
+      //return callback(error);
+    } else {
+      console.log(data);
+    }
+  })
+
+}
+
+
+
+
+module.exports.bye = (event, context, callback) => {
+  var bucket = event.Records[0].s3.bucket.name;
+  var fileName = event.Records[0].s3.object.key;
+
+  console.log(bucket)
+  console.log(fileName)
+
+  const params = {
+    Message: `A file named : ${fileName} has been deleted from the bucket: ${bucket} `,
+    TopicArn: "arn:aws:sns:us-east-1:389480544174:s3uploadTopic"
+  }
+  //console.log("params", params)
+  SNS.publish(params, function (err, data) {
+    if (err) {
+      console.error(err.stack);
+      //return callback(error);
+    } else {
+      console.log("sns msg sent", data);
+    }
+  })
+
+
+
+}
+
+
